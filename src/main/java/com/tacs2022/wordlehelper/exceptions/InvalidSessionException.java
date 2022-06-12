cb0eb3d4b2872e565e4512ded32bd89511c59d32
matiasanz <matias.l.anzorandia@gmail.com>
2022-06-12 03:44:04 -0300

    mensajes de excepciones se inlcuyen en body + cambio status de invalidSessionException a 404

--- src/main/java/com/tacs2022/wordlehelper/exceptions/InvalidSessionException.java
+++ src/main/java/com/tacs2022/wordlehelper/exceptions/InvalidSessionException.java
@@ -3,7 +3,7 @@ package com.tacs2022.wordlehelper.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class InvalidSessionException extends RuntimeException {
     public InvalidSessionException(){
         super("Invalid session");
